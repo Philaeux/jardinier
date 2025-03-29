18a308960173fe219a4cdb03a07675812bd810fa
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2025-03-29 19:42:56 +0100

    Basic chart

--- jardinier-frontend/src/app/components/index/index.component.ts
+++ jardinier-frontend/src/app/components/index/index.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../../services/api.service';
+import { Component, HostListener, OnInit } from '@angular/core'
+import { ApiService } from '../../services/api.service'
+import { NgxChartsModule } from "@swimlane/ngx-charts"
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [
+    NgxChartsModule
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
@@ -14,7 +17,7 @@ export class IndexComponent implements OnInit {
   callBackend: string = "Not processed"
 
   ngOnInit(): void {
-    
+
     /** Example of call to the backend */
     this.apiService.querySuccessExample().subscribe((response) => {
       if (response.data.querySuccessExample.__typename == "ApiSuccess") {
@@ -23,4 +26,67 @@ export class IndexComponent implements OnInit {
     })
 
   }
+
+  view: [number, number] = [window.innerWidth * 0.95, window.innerHeight / 2 * 0.95];
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.view = [window.innerWidth * 0.95, window.innerHeight / 2 * 0.95]
+  }
+
+  mockDataTemp = [
+    {
+      "name": "Température",
+      "series": [
+        {
+          "value": 20,
+          "name": "2016-09-19T21:17:52.618Z"
+        },
+        {
+          "value": 25,
+          "name": "2016-09-14T09:12:52.243Z"
+        },
+        {
+          "value": 30,
+          "name": "2016-09-24T03:36:21.195Z"
+        },
+        {
+          "value": 28,
+          "name": "2016-09-19T23:01:26.514Z"
+        },
+        {
+          "value": 22,
+          "name": "2016-09-19T14:48:19.814Z"
+        }
+      ]
+    },
+  ]
+
+  mockDataHum = [
+    {
+      "name": "Humidité",
+      "series": [
+        {
+          "value": 20,
+          "name": "2016-09-19T21:17:52.618Z"
+        },
+        {
+          "value": 25,
+          "name": "2016-09-14T09:12:52.243Z"
+        },
+        {
+          "value": 30,
+          "name": "2016-09-24T03:36:21.195Z"
+        },
+        {
+          "value": 28,
+          "name": "2016-09-19T23:01:26.514Z"
+        },
+        {
+          "value": 22,
+          "name": "2016-09-19T14:48:19.814Z"
+        }
+      ]
+    },
+  ]
 }
